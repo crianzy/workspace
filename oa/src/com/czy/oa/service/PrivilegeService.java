d1e7ce1804c3a0fa62d5a665959875a489c3dfcf
crianzy <chen930724@gmail.com>
2013-06-08 14:06:40 +0800

    c

---
+++ oa/src/com/czy/oa/service/PrivilegeService.java
@@ -0,0 +1,8 @@
+package com.czy.oa.service;
+
+import com.czy.oa.base.BaseDao;
+import com.czy.oa.domain.Privilege;
+
+public interface PrivilegeService extends BaseDao<Privilege> {
+
+}
---
+++ oa/src/com/czy/oa/service/impl/PrivilegeServiceImpl.java
@@ -0,0 +1,12 @@
+package com.czy.oa.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.czy.oa.base.BaseDaoImpl;
+import com.czy.oa.domain.Privilege;
+import com.czy.oa.service.PrivilegeService;
+
+@Service
+public class PrivilegeServiceImpl extends BaseDaoImpl<Privilege> implements PrivilegeService {
+
+}
