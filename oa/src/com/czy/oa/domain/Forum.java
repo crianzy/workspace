97aea17d92dca0df901c3e37c4c1d8bf859f3657
crianzy <chen930724@gmail.com>
2013-06-14 19:50:34 +0800

    c

---
+++ oa/src/com/czy/oa/domain/Forum.java
@@ -0,0 +1,50 @@
+package com.czy.oa.domain;
+
+import java.io.Serializable;
+
+/**
+ * ∞ÂøÈ
+ * @author chen
+ *
+ */
+public class Forum implements Serializable{
+
+	private static final long serialVersionUID = -1368861124984902738L;
+	private Long id;
+	private String name;
+	private String description;
+	private int position;// ≈≈–Ú”√µƒŒª÷√∫≈
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+}
